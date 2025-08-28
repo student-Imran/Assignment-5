c74b116515c21e95add0e33034cf6f6282698278
student-Imran <ahmedimranmd55@gmail.com>
2025-08-28 11:37:32 +0600

    Card with Alert responsive done

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 // DOM of History and Coin
 
-const callButtons = document.querySelectorAll("#call-btn")
+const callButtons = document.querySelectorAll("#call-btn");
+const customAlert = document.getElementById("customAlert");
+
 
 callButtons.forEach((btn)=>{
   btn.addEventListener("click",function(){
-    const currentCoin =  document.getElementById("current-coin")
-    currentCoin.innerText = parseInt(currentCoin.innerText)-20;
-    const organizationName = btn.parentElement.parentElement.children[0]
-    const organizationNumber = btn.parentElement.parentElement.children[2]
-    const div = document.createElement("div");
-    const date = new Date().toLocaleTimeString();
+     const organizationName = btn.parentElement.parentElement.children[0]
+     const organizationNumber = btn.parentElement.parentElement.children[2]
+     const divAlert = document.createElement("div");
+     divAlert.innerHTML = `
+      <div class="div-1">
+           <p>emergency-service.netify.app says</p>
+        </div><br>
+        <div class="div-2 flex gap-3">
+          <div class="icon-div">
+           <i class="fa-solid fa-phone-volume" style="color: #fc031c;"></i>
+          </div>
+          <div class="calling flex gap-3">
+             <h2>Calling ${organizationName.innerHTML}</h2>
+             <p>${organizationNumber.innerHTML}</p>
+          </div>
+        </div><br>
+        <div class="div-3 flex justify-between">
+          <div>
+
+          </div>
+          <div>
+            <button id="alertOK-btn" class="bg-pink-300 text-black w-16 h-10 rounded-xl">OK</button>
+          </div>
+        </div>
+     `
+     customAlert.style.background= 'black'
+
+     customAlert.appendChild(divAlert)
+     customAlert.style.display = 'block';
+
+     const alertOkBtn = document.getElementById("alertOK-btn");
+     
+     alertOkBtn.addEventListener("click",function(){
+      customAlert.textContent ='';
+      customAlert.style.background= 'white'
+      customAlert.style = 'z=-1'
+      const currentCoin =  document.getElementById("current-coin")
+      currentCoin.innerText = parseInt(currentCoin.innerText)-20;
+      const div = document.createElement("div");
+      const date = new Date().toLocaleTimeString();
       div.innerHTML = `
-   <div id="addDiv" class="history flex justify-between bg-[#fafafa] h-20 items-center p-2  rounded-sm mb-3">
+      <div id="addDiv" class="history flex justify-between bg-[#fafafa] h-20 items-center p-2  rounded-sm mb-3">
            <div class="history-information w-2/3">
                <h1 class="font-bold">${organizationName.innerHTML}</h1>
                <div class="number">
@@ -24,12 +60,19 @@ callButtons.forEach((btn)=>{
            </div>
            
   `
-  document.getElementById("addDiv").appendChild(div);
+         document.getElementById("addDiv").appendChild(div);
+
+     })
+   
+    
+   
+    
+      
     
   })
 })
 
-/* DOM of Heart */
+//  DOM of Heart 
 
 const hearts = document.querySelectorAll(".fa-heart")
 
